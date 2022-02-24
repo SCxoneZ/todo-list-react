d458f6ead5ab4d6c01db51148369e80ace9da1a5
SCxoneZ <stevenniuschandraa@gmail.com>
2022-02-24 23:29:49 +0700

    able to add todo lists, but the button still not working

--- src/app.js
+++ src/app.js
@@ -1,1 +1,59 @@
-const el = (<h1>Hello World</h1>);
+const container = document.querySelector('.container');
+/*
+      <div className="card">
+        <div className="card-content">Lorem ipsum dolor.</div>
+        <div className="card-buttons">
+          <button type="button" className="btn-done"></button>
+          <button type="button" className="btn-delete"></button>
+          <button type="button" className="btn-edit"></button>
+        </div>
+*/
+function App() {
+  const [activity, setActivity] = React.useState('');
+  const [todos, setTodos] = React.useState([]);
+  const inputEl = React.useRef(null);
+  
+  function addHandler(){
+    const newTodo = {
+      id: Date.now(),
+      isDone: false,
+      activity
+    };
+    const updatedTodos = [...todos, newTodo];
+    setTodos(updatedTodos);
+    console.log(newTodo);
+    console.log(updatedTodos);
+  }
+  
+  return (
+
+    <>
+    <section className="form">
+      <input type="text" placeholder="Enter a todo..." onChange={
+        function(e){
+          setActivity(e.target.value);
+        }
+      }/>
+      <button type="button" onClick={addHandler} ref={inputEl}>Add</button>
+    </section>
+    <div className="cards">
+      {todos.map((todo) => {
+        return (
+        
+        <div key={todo.id} className="card">
+        <div className="card-content">{todo.activity}</div>
+        <div className="card-buttons">
+          <button type="button" className="btn-done"></button>
+          <button type="button" className="btn-delete"></button>
+          <button type="button" className="btn-edit"></button>
+        </div>
+        </div>
+        );
+      })}
+    </div>
+    </>
+
+  );
+}
+
+ReactDOM.render(<App/>, container);
