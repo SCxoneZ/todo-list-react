eed75d2d47295af2a0c4169fc48c7ff55a8188e0
SCxoneZ <stevenniuschandraa@gmail.com>
2022-02-25 13:45:47 +0700

    its finished, i don't know what to type :-)

--- src/app.js
+++ src/app.js
@@ -1,19 +1,11 @@
 const container = document.querySelector('.container');
-/*
-      <div className="card">
-        <div className="card-content">Lorem ipsum dolor.</div>
-        <div className="card-buttons">
-          <button type="button" className="btn-done"></button>
-          <button type="button" className="btn-delete"></button>
-          <button type="button" className="btn-edit"></button>
-        </div>
-*/
 function App() {
   const [activity, setActivity] = React.useState('');
   const [todos, setTodos] = React.useState([]);
   const inputEl = React.useRef(null);
   
   function addHandler(){
+    if(!activity) return;
     const newTodo = {
       id: Date.now(),
       isDone: false,
@@ -21,31 +13,58 @@ function App() {
     };
     const updatedTodos = [...todos, newTodo];
     setTodos(updatedTodos);
-    console.log(newTodo);
-    console.log(updatedTodos);
+    setActivity('');
+    }
+  
+  function deleteHandler(todoId){
+    const updatedTodos = todos.filter((el) => {
+      return el.id !== todoId;
+    });
+    setTodos(updatedTodos);
+  }
+  
+  function editHandler(todoId){
+    const newTodos = [...todos];
+    const currentIndex = todos.findIndex(el => {
+      return todoId == el.id;
+    });
+    const edited = prompt('Masukkan Teks Yang akan diedit...', newTodos[currentIndex].activity);
+    if(edited == null) return;
+    newTodos[currentIndex].activity = edited;
+    setTodos(newTodos);
+  }
+  
+  function doneHandler(todoId){
+    const newTodos = [...todos];
+    const currentIndex = todos.findIndex(el => {
+      return todoId == el.id;
+    });
+    newTodos[currentIndex].isDone = !newTodos[currentIndex].isDone;
+    setTodos(newTodos);
   }
   
   return (
 
     <>
     <section className="form">
-      <input type="text" placeholder="Enter a todo..." onChange={
+      <input type="text" ref={inputEl} placeholder="Enter a todo..." onChange={
         function(e){
           setActivity(e.target.value);
         }
       }/>
-      <button type="button" onClick={addHandler} ref={inputEl}>Add</button>
+      <button type="button" onClick={addHandler}>Add</button>
     </section>
     <div className="cards">
       {todos.map((todo) => {
+        const contentClass = `card-content ${todo.isDone && 'done'}`;
         return (
         
         <div key={todo.id} className="card">
-        <div className="card-content">{todo.activity}</div>
+        <div className={contentClass}>{todo.activity}</div>
         <div className="card-buttons">
-          <button type="button" className="btn-done"></button>
-          <button type="button" className="btn-delete"></button>
-          <button type="button" className="btn-edit"></button>
+          <button type="button" className="btn-done" onClick={doneHandler.bind(this, todo.id)}></button>
+          <button type="button" className="btn-delete" onClick={deleteHandler.bind(this, todo.id)}></button>
+          <button type="button" className="btn-edit" onClick={editHandler.bind(this, todo.id)}></button>
         </div>
         </div>
         );
